863cc19ab079f11b4bdc2088acac346f8efc0b06
Adrian Looney <adrian.looney@gmail.com>
2021-11-30 16:50:29 +0000

    initial login page setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,21 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      white: '#ffffff', 
+      blue: {
+        medium: '#005c98'
+      },
+      black: {
+        light: '#262626',
+        faded: '#00000059'
+      },
+      gray: {
+        base: '#616161',
+        background: '#fafafa',
+        primary: '#dbdbdb'
+      },
+    }
   },
   variants: {
     extend: {},
